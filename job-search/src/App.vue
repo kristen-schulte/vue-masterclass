5359b0ba6a7e00c7f36a60d8a8116e97c7c4e152
kristen <kristen.schulte@huskers.unl.edu>
2022-07-13 16:49:08 -0500

    Add fonts. Start on main nav.

--- job-search/src/App.vue
+++ job-search/src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>Hello Vue</h1>
+  <div>
+    <MainNav />
+  </div>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: [MainNav],
 };
 </script>
---
+++ job-search/src/components/MainNav.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="w-full text-sm">Main Nav</header>
+</template>
+<script>
+export default { name: "MainNav" };
+</script>
