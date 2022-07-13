796bd9f4ea75767f2657ad2c8ede3169759ce51a
kristen <kristen.schulte@huskers.unl.edu>
2022-07-13 17:35:00 -0500

    Add main nav.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
+  <MainNav />
 </template>
 
 <script>
+import MainNav from "./components/MainNav.vue";
 export default {
   name: "App",
+  components: [MainNav],
 };
 </script>
