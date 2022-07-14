c06615d9da88f6d2554b7f477949988f6127598c
kristen <kristen.schulte@huskers.unl.edu>
2022-07-14 07:40:06 -0500

    Using test attribute for find.

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -13,6 +13,7 @@
               v-for="item in menuItems"
               :key="item"
               class="h-full ml-9 first:ml-0"
+              data-test="main-nav-list-item"
             >
               <a href="" class="flex items-center h-full py-2.5">{{ item }}</a>
             </li>
