b946cf82718f4356604a7359e6323a1f28a9a39e
kristen <kristen.schulte@huskers.unl.edu>
2022-07-14 15:11:02 -0500

    Computed property example.

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,8 +1,10 @@
 <template>
-  <button
-    class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
+  <button :class="{ primary: primary }">
     {{ text }}
+  </button>
+
+  <button :class="{ primary: primary }" @click="doubleHeight">
+    {{ area }}
   </button>
 </template>
 
@@ -10,5 +12,31 @@
 export default {
   name: "ActionButton",
   props: ["text"],
+  data() {
+    return {
+      primary: true,
+      width: 10,
+      height: 5,
+    };
+  },
+  computed: {
+    area() {
+      return this.width * this.height;
+    },
+  },
+  methods: {
+    doubleHeight() {
+      this.height = this.height * 2;
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply px-5 py-3 rounded font-medium;
+}
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
