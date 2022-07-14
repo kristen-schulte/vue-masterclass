ea9be5b644da29df185a88adecf6fd676a74343d
kristen <kristen.schulte@huskers.unl.edu>
2022-07-14 07:38:29 -0500

    Stop git tracking coverage reports.

--- src/assets/tests/unit/components/MainNav.test.js
+++ src/assets/tests/unit/components/MainNav.test.js
@@ -6,4 +6,19 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Brand Careers");
   });
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const texts = navigationMenuItems.map((i) => i.text());
+    expect(texts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Brand",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });
