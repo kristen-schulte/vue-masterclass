83ecbba0d779b8f628065e8f6709159282028cd2
kristen <kristen.schulte@huskers.unl.edu>
2022-07-14 06:33:40 -0500

    Discussed method option available in mount method.

--- src/assets/tests/unit/components/MainNav.test.js
+++ src/assets/tests/unit/components/MainNav.test.js
@@ -3,7 +3,13 @@ import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
   it("displays the company name", () => {
-    const wrapper = mount(MainNav);
-    expect(wrapper.text()).toMatch("Brand Careers");
+    const wrapper = mount(MainNav, {
+      data() {
+        return {
+          company: "Super Corp",
+        };
+      },
+    });
+    expect(wrapper.text()).toMatch("Super Corp");
   });
 });
