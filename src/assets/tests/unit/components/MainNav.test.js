31ed4cfee6dda46060d0700410c3e7fb593a31e3
kristen <kristen.schulte@huskers.unl.edu>
2022-07-14 06:51:07 -0500

    Revert test.

--- src/assets/tests/unit/components/MainNav.test.js
+++ src/assets/tests/unit/components/MainNav.test.js
@@ -2,11 +2,8 @@ import { mount } from "@vue/test-utils";
 import MainNav from "@/components/MainNav.vue";
 
 describe("MainNav", () => {
-  it("displays the company name", async () => {
+  it("displays the company name", () => {
     const wrapper = mount(MainNav);
-    await wrapper.setData({
-      company: "Super Corp",
-    });
-    expect(wrapper.text()).toMatch("Super Corp");
+    expect(wrapper.text()).toMatch("Brand Careers");
   });
 });
