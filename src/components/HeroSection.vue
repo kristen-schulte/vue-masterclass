47de1528259f7e519670c359ea6394160da99bf3
kristen <kristen.schulte@huskers.unl.edu>
2022-07-15 09:28:12 -0500

    Grid columns; life cycle hooks.

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,11 +1,19 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20">Hero</section>
+    <section class="flex flex-col h-screen pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-700">1</div>
+        <div class="col-start-2 col-span-5 border border-blue-700">5</div>
+        <div class="col-start-7 col-span-5 border border-blue-700">5</div>
+        <div class="col-start-12 col-span-1 border border-blue-700">1</div>
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
 export default {
   name: "HeroSection",
+  beforeCreate() {},
 };
 </script>
