5556bb2fbb16b277e175cd0fb8372c031e10db1e
kristen <kristen.schulte@huskers.unl.edu>
2022-07-15 06:21:41 -0500

    Detailed specs on props.

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -28,7 +28,6 @@
           <action-button
             v-else
             text="Sign In"
-            type="primary"
             data-test="login-button"
             @click="swapUserLogin"
           />
