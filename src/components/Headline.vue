a460b36b28f81529c6984201b8d7fdc1804990e0
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 06:02:22 -0500

    Add interval action verb change.

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h3>Build for everyone</h3>
+    <h3>{{ action }} for everyone</h3>
     <h2>Find your next job at Brand.</h2>
   </section>
 </template>
@@ -10,11 +10,25 @@ export default {
   name: "HeadlineComponent",
   data() {
     return {
-      sample: "Hello",
+      action: "Build",
+      interval: null,
     };
   },
   created() {
-    console.log("created", this.sample);
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const index = actions.indexOf(this.action);
+        const nextIndex = (index + 1) % 4;
+        this.action = actions[nextIndex];
+      }, 3000);
+    },
   },
 };
 </script>
