481c8546a14eb3084c1d0cee25cc110e062d6301
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 09:34:00 -0500

    Search box start.

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,5 +1,9 @@
 <template>
-  <form>form text</form>
+  <form
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+  </form>
 </template>
 
 <script>
