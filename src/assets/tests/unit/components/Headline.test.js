2857fb4d3bab3cc1cbfd7b537f440e0307d6d0aa
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 08:13:26 -0500

    Refactor tests.

--- src/assets/tests/unit/components/Headline.test.js
+++ src/assets/tests/unit/components/Headline.test.js
@@ -3,28 +3,30 @@ import { mount } from "@vue/test-utils";
 import Headline from "@/components/Headline.vue";
 
 describe("Headline", () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     jest.useFakeTimers("legacy");
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("displays introductory action verb", () => {
     const wrapper = mount(Headline);
     const phrase = wrapper.find("[data-test='action-phrase']");
     expect(phrase.text()).toBe("Build for everyone");
-    jest.useRealTimers();
   });
 
   it("changes action verb at a consistent interval", () => {
-    jest.useFakeTimers("legacy");
     mount(Headline);
     expect(setInterval).toHaveBeenCalled();
-    jest.useRealTimers();
   });
 
   it("swaps action verb after first interval", async () => {
-    jest.useFakeTimers("legacy");
     const wrapper = mount(Headline);
     jest.runOnlyPendingTimers();
     await nextTick();
     const phrase = wrapper.find("[data-test='action-phrase']");
     expect(phrase.text()).toBe("Create for everyone");
-    jest.useRealTimers();
   });
 });
