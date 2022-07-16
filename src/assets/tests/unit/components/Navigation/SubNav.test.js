70826758d0d4ca3ffce69ee1737d72527afdaaae
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 15:48:25 -0500

    Compute on job page and test.

--- src/assets/tests/unit/components/Navigation/SubNav.test.js
+++ src/assets/tests/unit/components/Navigation/SubNav.test.js
@@ -1,22 +1,25 @@
 import { mount } from "@vue/test-utils";
 import SubNav from "@/components/Navigation/SubNav";
 
+function createConfig(routeName) {
+  return {
+    global: {
+      mocks: {
+        $route: {
+          name: routeName,
+        },
+      },
+      stubs: {
+        FontAwesomeIcon: true,
+      },
+    },
+  };
+}
+
 describe("SubNav", () => {
   describe("when user is on job page", () => {
     it("displays job count", () => {
-      const wrapper = mount(SubNav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
-        },
-      });
-
+      const wrapper = mount(SubNav, createConfig("JobResults"));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(true);
     });
@@ -24,19 +27,7 @@ describe("SubNav", () => {
 
   describe("when user is not on job page", () => {
     it("does not display job count", () => {
-      const wrapper = mount(SubNav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
-        },
-      });
-
+      const wrapper = mount(SubNav, createConfig("Home"));
       const jobCount = wrapper.find("[data-test='job-count']");
       expect(jobCount.exists()).toBe(false);
     });
