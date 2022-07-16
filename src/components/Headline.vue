c073e3f88367bf79fe140e43f3cfed829eda8e26
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 05:50:00 -0500

    Start headline.

---
+++ src/components/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h3>Build for everyone</h3>
+    <h2>Find your next job at Brand.</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HeadlineComponent",
+  data() {
+    return {
+      sample: "Hello",
+    };
+  },
+  created() {
+    console.log("created", this.sample);
+  },
+};
+</script>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,7 +3,9 @@
     <section class="flex flex-col h-screen pt-10 pb-20">
       <div class="grid grid-cols-12">
         <div class="col-start-1 col-span-1 border border-blue-700">1</div>
-        <div class="col-start-2 col-span-5 border border-blue-700">5</div>
+        <div class="col-start-2 col-span-5 border border-blue-700">
+          <headline />
+        </div>
         <div class="col-start-7 col-span-5 border border-blue-700">5</div>
         <div class="col-start-12 col-span-1 border border-blue-700">1</div>
       </div>
@@ -12,8 +14,11 @@
 </template>
 
 <script>
+import Headline from "./Headline.vue";
 export default {
   name: "HeroSection",
-  beforeCreate() {},
+  components: {
+    Headline,
+  },
 };
 </script>
