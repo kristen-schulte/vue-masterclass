4730b0e316e53ea71e1c6a5ff207da21546a82b6
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 06:09:54 -0500

    Apply CSS class with color change.

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,6 +1,8 @@
 <template>
   <section>
-    <h3>{{ action }} for everyone</h3>
+    <h3>
+      <span :class="actionClasses">{{ action }}</span> <br />for everyone
+    </h3>
     <h2>Find your next job at Brand.</h2>
   </section>
 </template>
@@ -13,6 +15,16 @@ export default {
       action: "Build",
       interval: null,
     };
+  },
+  computed: {
+    actionClasses() {
+      return {
+        build: this.action === "Build",
+        create: this.action === "Create",
+        design: this.action === "Design",
+        code: this.action === "Code",
+      };
+    },
   },
   created() {
     this.changeTitle();
@@ -32,3 +44,19 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
