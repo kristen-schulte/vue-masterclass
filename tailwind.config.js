32c84f372f723a23bfc328f6456249abc59f4bb8
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 09:22:30 -0500

    Custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
       },
       colors: {
         "brand-gray-1": "#dadce0",
+        "brand-gray-2": "#f8f9fa",
+        "brand-gray-3": "#80868b",
         "brand-blue-1": "#1967D2",
-        "brand-green-1": "#137333",
         "brand-blue-2": "#4285f4",
+        "brand-green-1": "#137333",
       },
       boxShadow: {
         blue: "0 0 3px 3px #4285f4",
