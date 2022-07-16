36faa5d7a66d35538c9b5c3e4f092a2f77dbba13
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 14:53:41 -0500

    Routing.

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,8 +1,16 @@
 <template>
-  <h1>Job Results View</h1>
+  <div>
+    <h1>Job Results View</h1>
+    <button @click="goToHome">Go to Home Page</button>
+  </div>
 </template>
 <script>
 export default {
   name: "JobResultsView",
+  methods: {
+    goToHome() {
+      this.$router.push({ name: "Home" });
+    },
+  },
 };
 </script>
