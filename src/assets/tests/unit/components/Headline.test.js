4e4d51a8a214b2596817d31d55efc3df16b70480
kristen <kristen.schulte@huskers.unl.edu>
2022-07-16 08:04:24 -0500

    More headline tests. Simulate timers and dom refresh.

--- src/assets/tests/unit/components/Headline.test.js
+++ src/assets/tests/unit/components/Headline.test.js
@@ -1,3 +1,4 @@
+import { nextTick } from "vue";
 import { mount } from "@vue/test-utils";
 import Headline from "@/components/Headline.vue";
 
@@ -7,6 +8,23 @@ describe("Headline", () => {
     const wrapper = mount(Headline);
     const phrase = wrapper.find("[data-test='action-phrase']");
     expect(phrase.text()).toBe("Build for everyone");
+    jest.useRealTimers();
+  });
+
+  it("changes action verb at a consistent interval", () => {
+    jest.useFakeTimers("legacy");
+    mount(Headline);
+    expect(setInterval).toHaveBeenCalled();
+    jest.useRealTimers();
+  });
+
+  it("swaps action verb after first interval", async () => {
+    jest.useFakeTimers("legacy");
+    const wrapper = mount(Headline);
+    jest.runOnlyPendingTimers();
+    await nextTick();
+    const phrase = wrapper.find("[data-test='action-phrase']");
+    expect(phrase.text()).toBe("Create for everyone");
     jest.useRealTimers();
   });
 });
