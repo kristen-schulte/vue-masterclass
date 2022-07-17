a07097d84a4a02a7613d53565c89da84d8a756c4
kristen <kristen.schulte@huskers.unl.edu>
2022-07-17 16:26:23 -0500

    Add page navigation.

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -8,6 +8,25 @@
         data-test="job-listing"
       />
     </ol>
+    <div class="mt-8 mx-auto">
+      <div class="flex flex-row flex-nowrap">
+        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+        <div class="flex items-center justify-center">
+          <router-link
+            v-if="previousPage"
+            :to="{ name: 'JobResults', query: { page: previousPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Previous</router-link
+          >
+          <router-link
+            v-if="nextPage"
+            :to="{ name: 'JobResults', query: { page: nextPage } }"
+            class="mx-3 text-sm font-semibold text-brand-blue-1"
+            >Next</router-link
+          >
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -26,8 +45,19 @@ export default {
     };
   },
   computed: {
+    currentPage() {
+      return Number.parseInt(this.$route.query.page || "1");
+    },
+    previousPage() {
+      return this.currentPage > 1 ? this.currentPage - 1 : undefined;
+    },
+    nextPage() {
+      return this.currentPage < this.jobs.length / 10
+        ? this.currentPage + 1
+        : undefined;
+    },
     displayedJobs() {
-      const page = Number.parseInt(this.$route.query.page || "1");
+      const page = this.currentPage;
       const start = (page - 1) * 10;
       const end = 10 * page;
       return this.jobs.slice(start, end);
