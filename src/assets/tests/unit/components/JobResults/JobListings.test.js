53ab26f2965f19f9439f2be71e649551e7a6ed80
kristen <kristen.schulte@huskers.unl.edu>
2022-07-17 16:02:04 -0500

    Fix tests.

--- src/assets/tests/unit/components/JobResults/JobListings.test.js
+++ src/assets/tests/unit/components/JobResults/JobListings.test.js
@@ -3,16 +3,34 @@ import axios from "axios";
 jest.mock("axios");
 import JobListings from "@/components/JobResults/JobListings.vue";
 
+function createRoute(page = "5") {
+  return {
+    query: {
+      page,
+    },
+  };
+}
+
+function createConfig($route) {
+  return {
+    global: {
+      mocks: {
+        $route,
+      },
+    },
+  };
+}
+
 describe("JobListings", () => {
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    shallowMount(JobListings);
+    shallowMount(JobListings, createConfig(createRoute()));
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job listing for a maximum of 10 jobs", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
-    const wrapper = shallowMount(JobListings);
+    const wrapper = shallowMount(JobListings, createConfig(createRoute("1")));
     await flushPromises();
     const listings = wrapper.findAll("[data-test='job-listing']");
     expect(listings).toHaveLength(10);
