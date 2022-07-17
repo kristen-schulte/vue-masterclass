462a242a0aed16d6043a1bfa85bb87d504a4c3db
kristen <kristen.schulte@huskers.unl.edu>
2022-07-17 14:23:00 -0500

    Tests for job listing.

--- src/assets/tests/unit/components/JobResults/JobListing.test.js
+++ src/assets/tests/unit/components/JobResults/JobListing.test.js
@@ -38,4 +38,27 @@ describe("JobListing", () => {
     );
     expect(wrapper.text()).toMatch("AirBnb");
   });
+
+  it("renders job locations", () => {
+    const wrapper = mount(
+      JobListing,
+      createConfig(createJobProps({ locations: ["Jacksonville", "Orlando"] }))
+    );
+    expect(wrapper.text()).toMatch("Jacksonville");
+    expect(wrapper.text()).toMatch("Orlando");
+  });
+
+  it("renders job qualifications", () => {
+    const wrapper = mount(
+      JobListing,
+      createConfig(createJobProps({ minimumQualifications: ["Succeed"] }))
+    );
+    expect(wrapper.text()).toMatch("Succeed");
+  });
+
+  it("links to individual job page", () => {
+    const wrapper = mount(JobListing, createConfig(createJobProps({ id: 15 })));
+    const link = wrapper.findComponent(RouterLinkStub);
+    expect(link.props("to")).toBe("/jobs/results/15");
+  });
 });
