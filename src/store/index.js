6785b38faf398bbd70cf99d794853adee4cbb98d
kristen <kristen.schulte@huskers.unl.edu>
2022-07-19 10:42:45 -0500

    Move logged in to store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,27 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export const state = () => {
+  return {
+    isLoggedIn: false,
+  };
+};
+
+export const mutations = {
+  LOGIN_USER(state) {
+    state.isLoggedIn = true;
+  },
+  LOGOUT_USER(state) {
+    state.isLoggedIn = false;
+  },
+};
+
+const store = createStore({
+  state,
+  // getters: {},
+  mutations,
+  strict: process.env.NODE_ENV !== "production",
+  // actions: {},
+  // modules: {},
 });
+
+export default store;
