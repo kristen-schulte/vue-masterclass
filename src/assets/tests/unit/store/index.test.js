be08ad2592d05eab8c7ff25378861feb1fffeeb4
kristen <kristen.schulte@huskers.unl.edu>
2022-07-26 15:12:01 -0500

    Test getter.

--- src/assets/tests/unit/store/index.test.js
+++ src/assets/tests/unit/store/index.test.js
@@ -1,4 +1,4 @@
-import { state, mutations, actions } from "@/store";
+import { state, mutations, actions, getters } from "@/store";
 import getJobs from "@/api/getJobs";
 jest.mock("@/api/getJobs");
 
@@ -12,61 +12,77 @@ describe("state", () => {
     const startingState = state();
     expect(startingState.jobs).toEqual([]);
   });
+});
 
-  describe("mutations", () => {
-    describe("LOGIN_USER", () => {
-      it("logs the user in", () => {
-        const state = { isLoggedIn: false };
-        mutations.LOGIN_USER(state);
-        expect(state).toEqual({ isLoggedIn: true });
-      });
-    });
-
-    describe("LOGOUT_USER", () => {
-      it("logs the user out", () => {
-        const state = { isLoggedIn: true };
-        mutations.LOGOUT_USER(state);
-        expect(state).toEqual({ isLoggedIn: false });
-      });
-    });
-
-    describe("RECEIVE_JOBS", () => {
-      it("receives jobs from API response", () => {
-        const state = { jobs: [] };
-        mutations.RECEIVE_JOBS(state, ["Job 1", "Job 2"]);
-        expect(state).toEqual({ jobs: ["Job 1", "Job 2"] });
-      });
+describe("mutations", () => {
+  describe("LOGIN_USER", () => {
+    it("logs the user in", () => {
+      const state = { isLoggedIn: false };
+      mutations.LOGIN_USER(state);
+      expect(state).toEqual({ isLoggedIn: true });
     });
   });
 
-  describe("actions", () => {
-    describe("FETCH_JOBS", () => {
-      beforeEach(() => {
-        getJobs.mockResolvedValue([
-          {
-            id: 1,
-            title: "Software Developer",
-          },
-        ]);
-      });
+  describe("LOGOUT_USER", () => {
+    it("logs the user out", () => {
+      const state = { isLoggedIn: true };
+      mutations.LOGOUT_USER(state);
+      expect(state).toEqual({ isLoggedIn: false });
+    });
+  });
 
-      it("makes request to fetch jobs", async () => {
-        const context = { commit: jest.fn() };
-        await actions.FETCH_JOBS(context);
-        expect(getJobs).toHaveBeenCalled();
-      });
+  describe("RECEIVE_JOBS", () => {
+    it("receives jobs from API response", () => {
+      const state = { jobs: [] };
+      mutations.RECEIVE_JOBS(state, ["Job 1", "Job 2"]);
+      expect(state).toEqual({ jobs: ["Job 1", "Job 2"] });
+    });
+  });
+});
 
-      it("sends message to save jobs in store", async () => {
-        const commit = jest.fn();
-        const context = { commit };
-        await actions.FETCH_JOBS(context);
-        expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
-          {
-            id: 1,
-            title: "Software Developer",
-          },
-        ]);
-      });
+describe("getters", () => {
+  describe("UNIQUE_ORGANIZATIONS", () => {
+    it("finds unique orgs from list of jobs", () => {
+      const state = {
+        jobs: [
+          { organization: "Google" },
+          { organization: "Amazon" },
+          { organization: "Google" },
+        ],
+      };
+      const result = getters.UNIQUE_ORGANIZATIONS(state);
+      expect(result).toEqual(new Set(["Google", "Amazon"]));
+    });
+  });
+});
+
+describe("actions", () => {
+  describe("FETCH_JOBS", () => {
+    beforeEach(() => {
+      getJobs.mockResolvedValue([
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
+    });
+
+    it("makes request to fetch jobs", async () => {
+      const context = { commit: jest.fn() };
+      await actions.FETCH_JOBS(context);
+      expect(getJobs).toHaveBeenCalled();
+    });
+
+    it("sends message to save jobs in store", async () => {
+      const commit = jest.fn();
+      const context = { commit };
+      await actions.FETCH_JOBS(context);
+      expect(commit).toHaveBeenCalledWith("RECEIVE_JOBS", [
+        {
+          id: 1,
+          title: "Software Developer",
+        },
+      ]);
     });
   });
 });
