5c652e6716a0849f7c805f3121301926d60b5360
kristen <kristen.schulte@huskers.unl.edu>
2022-07-26 08:48:35 -0500

    Make data availabile to slot parent.

---
+++ src/components/JobSearch/Spotlight.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="spot in spotlights" :key="spot.id">
+      <slot :spotlight="spot"></slot>
+    </li>
+  </ul>
+</template>
+<script>
+import axios from "axios";
+export default {
+  name: "Spotlight",
+  data() {
+    return {
+      spotlights: [],
+    };
+  },
+  async mounted() {
+    const baseUrl = process.env.VUE_APP_API_URL;
+    const url = `${baseUrl}/spotlights`;
+    const response = await axios.get(url);
+    this.spotlights = response.data;
+  },
+};
+</script>
