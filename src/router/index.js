cba2091773c2658f0be5cef23dc7adee54c79bc6
kristen <kristen.schulte@huskers.unl.edu>
2022-07-26 08:16:40 -0500

    Teams view.

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const HomeView = () => import("@/views/HomeView.vue");
 const JobResultsView = () => import("@/views/JobResultsView.vue");
 const JobView = () => import("@/views/JobView.vue");
+const TeamsView = () => import("@/views/TeamsView.vue");
 
 const routes = [
   { path: "/", name: "Home", component: HomeView },
   { path: "/jobs/results", name: "JobResults", component: JobResultsView },
   { path: "/jobs/results/:id", name: "JobListing", component: JobView },
+  { path: "/teams", name: "Teams", component: TeamsView },
 ];
 
 const router = createRouter({
---
+++ src/views/TeamsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <header-container />
+</template>
+
+<script>
+import HeaderContainer from "@/components/Shared/HeaderContainer.vue";
+export default {
+  name: "TeamsView",
+  components: {
+    HeaderContainer,
+  },
+};
+</script>
