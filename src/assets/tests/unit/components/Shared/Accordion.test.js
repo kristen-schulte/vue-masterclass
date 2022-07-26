4d44e972edac3b213e9741f2d3f30dc2f5eda15d
kristen <kristen.schulte@huskers.unl.edu>
2022-07-26 05:49:52 -0500

    Test default slot content.

--- src/assets/tests/unit/components/Shared/Accordion.test.js
+++ src/assets/tests/unit/components/Shared/Accordion.test.js
@@ -22,4 +22,19 @@ describe("Accordion", () => {
 
     expect(wrapper.text()).toMatch(testString);
   });
+
+  describe("when we do not provide custom slot content", () => {
+    it("renders default content", async () => {
+      const wrapper = mount(Accordion, {
+        global: { stubs: { FontAwesomeIcon: true } },
+        props: {
+          header: "Test Header",
+        },
+      });
+      const clickableArea = wrapper.find("[data-test='clickable-area']");
+      await clickableArea.trigger("click");
+
+      expect(wrapper.text()).toMatch("Default slot content.");
+    });
+  });
 });
