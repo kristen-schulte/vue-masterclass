018d073d7354f91205e7449b8d6123cd7de86b98
kristen <kristen.schulte@huskers.unl.edu>
2022-07-30 16:21:48 -0500

    Two new composables and tests.

--- src/store/composables.js
+++ src/store/composables.js
@@ -1,8 +1,22 @@
 import { computed } from "vue";
 import { useStore } from "vuex";
-import { FILTERED_JOBS } from "./constants";
+import {
+  FILTERED_JOBS,
+  UNIQUE_JOB_TYPES,
+  UNIQUE_ORGANIZATIONS,
+} from "./constants";
 
 export const useFilteredJobs = () => {
   const store = useStore();
   return computed(() => store.getters[FILTERED_JOBS]);
+};
+
+export const useUniqueJobTypes = () => {
+  const store = useStore();
+  return computed(() => store.getters[UNIQUE_JOB_TYPES]);
+};
+
+export const useUniqueOrgs = () => {
+  const store = useStore();
+  return computed(() => store.getters[UNIQUE_ORGANIZATIONS]);
 };
