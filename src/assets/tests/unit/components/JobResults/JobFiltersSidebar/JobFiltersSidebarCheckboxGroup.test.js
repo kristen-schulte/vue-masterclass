acf221de66a0e830b3747de8e6317f3f8c874665
kristen <kristen.schulte@huskers.unl.edu>
2022-07-30 18:23:09 -0500

    Combine separate types and orgs components.

--- src/assets/tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.js
+++ src/assets/tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.js
@@ -5,60 +5,61 @@ jest.mock("vuex");
 import { useRouter } from "vue-router";
 jest.mock("vue-router");
 
-import { useUniqueJobTypes } from "@/store/composables";
-jest.mock("@/store/composables");
+import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
 
-import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue";
-
-function createConfig() {
+function createConfig(props = {}) {
   return {
     global: {
       stubs: { FontAwesomeIcon: true },
     },
+    props: {
+      header: "Header",
+      uniqueValues: new Set(["Value-A", "Value-B"]),
+      mutations: "mutation",
+      ...props,
+    },
   };
 }
 
-describe("JobFiltersSidebarJobTypes", () => {
-  it("renders unique list of job types for filtering jobs", async () => {
-    useUniqueJobTypes.mockReturnValue(new Set(["Type-1", "Type-2"]));
-    const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
-
+describe("JobFiltersSidebarCheckboxGroup", () => {
+  it("renders unique list of values for filtering jobs", async () => {
+    const wrapper = mount(JobFiltersSidebarCheckboxGroup, createConfig());
     const clickableArea = wrapper.find("[data-test='clickable-area']");
     await clickableArea.trigger("click");
-    const typeLabels = wrapper.findAll("[data-test='job-type']");
+    const typeLabels = wrapper.findAll("[data-test='value']");
     const types = typeLabels.map((node) => node.text());
-    expect(types).toEqual(["Type-1", "Type-2"]);
+    expect(types).toEqual(["Value-A", "Value-B"]);
   });
 
   describe("when user clicks checkbox", () => {
-    it("communicates that user has selected checkbox for job type", async () => {
+    it("communicates that user has selected checkbox for value", async () => {
       useRouter.mockReturnValue({ push: jest.fn() });
       const commit = jest.fn();
       useStore.mockReturnValue({ commit });
-      useUniqueJobTypes.mockReturnValue(new Set(["Type-1", "Type-2"]));
-
-      const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
-
+      const props = {
+        mutation: "SOME_MUTATION",
+      };
+      const wrapper = mount(
+        JobFiltersSidebarCheckboxGroup,
+        createConfig(props)
+      );
       const clickableArea = wrapper.find("[data-test='clickable-area']");
       await clickableArea.trigger("click");
-      const input1 = wrapper.find("[data-test='Type-1']");
+      const input1 = wrapper.find("[data-test='Value-A']");
       await input1.setChecked();
-      expect(commit).toHaveBeenCalledWith("ADD_SELECTED_JOB_TYPES", ["Type-1"]);
+      expect(commit).toHaveBeenCalledWith("SOME_MUTATION", ["Value-A"]);
     });
   });
 
   it("navigates user to home job results page", async () => {
     useStore.mockReturnValue({ commit: jest.fn() });
-    useUniqueJobTypes.mockReturnValue(new Set(["Type-1", "Type-2"]));
     const push = jest.fn();
     useRouter.mockReturnValue({ push });
-    const wrapper = mount(JobFiltersSidebarJobTypes, createConfig());
-
+    const wrapper = mount(JobFiltersSidebarCheckboxGroup, createConfig());
     const clickableArea = wrapper.find("[data-test='clickable-area']");
     await clickableArea.trigger("click");
-    const input1 = wrapper.find("[data-test='Type-1']");
+    const input1 = wrapper.find("[data-test='Value-A']");
     await input1.setChecked();
-
     expect(push).toHaveBeenCalledWith({ name: "JobResults" });
   });
 });
