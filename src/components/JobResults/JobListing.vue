286b55a8eebe41810f88d6896d797d2e744ee792
kristen <kristen.schulte@huskers.unl.edu>
2022-07-31 10:31:26 -0500

    Refactor job listing to use composables API.

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -49,6 +49,7 @@
 </template>
 
 <script>
+import { computed } from "vue";
 export default {
   name: "JobListing",
   props: {
@@ -57,10 +58,9 @@ export default {
       required: true,
     },
   },
-  computed: {
-    jobPageLink() {
-      return `/jobs/results/${this.job.id}`;
-    },
+  setup(props) {
+    const jobPageLink = computed(() => `/jobs/results/${props.job.id}`);
+    return { jobPageLink };
   },
 };
 </script>
