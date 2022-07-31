7ce8a1aaf781a1544dbc6615ec461cfb2b8df7b9
kristen <kristen.schulte@huskers.unl.edu>
2022-07-31 14:24:02 -0500

    Convert job listing to ts.

--- src/components/JobResults/JobListing.vue
+++ src/components/JobResults/JobListing.vue
@@ -48,13 +48,15 @@
   </li>
 </template>
 
-<script>
-import { computed } from "vue";
-export default {
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+import { Job } from "@/api/types";
+
+export default defineComponent({
   name: "JobListing",
   props: {
     job: {
-      type: Object,
+      type: Object as PropType<Job>,
       required: true,
     },
   },
@@ -62,5 +64,5 @@ export default {
     const jobPageLink = computed(() => `/jobs/results/${props.job.id}`);
     return { jobPageLink };
   },
-};
+});
 </script>
