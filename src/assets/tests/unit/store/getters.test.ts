0c121c3e2961ecd2924b1a4c3b70102263f94019
kristen <kristen.schulte@huskers.unl.edu>
2022-07-31 16:42:49 -0500

    Add include job by degree to filtering logic.

--- src/assets/tests/unit/store/getters.test.ts
+++ src/assets/tests/unit/store/getters.test.ts
@@ -72,14 +72,36 @@ describe("getters", () => {
     });
   });
 
+  describe("INCLUDE_JOB_DEGREE", () => {
+    describe("when no degrees selected", () => {
+      it("includes job", () => {
+        const startingState = createState({ selectedDegrees: [] });
+        const job = createJob({ degree: "Bachelors" });
+        const result = getters.INCLUDE_JOB_DEGREE(startingState)(job);
+        expect(result).toBe(true);
+      });
+    });
+
+    it("identifies if job is of a degree", () => {
+      const startingState = createState({
+        selectedDegrees: ["Master's", "Bachelors"],
+      });
+      const job = createJob({ degree: "Bachelors" });
+      const result = getters.INCLUDE_JOB_DEGREE(startingState)(job);
+      expect(result).toBe(true);
+    });
+  });
+
   describe("FILTERED_JOBS", () => {
-    it("filters jobs by organization and job type", () => {
+    it("filters jobs by organization, job type, and degree", () => {
       const INCLUDE_JOB_ORG = jest.fn().mockReturnValue(true);
       const INCLUDE_JOB_TYPE = jest.fn().mockReturnValue(true);
+      const INCLUDE_JOB_DEGREE = jest.fn().mockReturnValue(true);
 
       const mockGetters = {
         INCLUDE_JOB_ORG,
         INCLUDE_JOB_TYPE,
+        INCLUDE_JOB_DEGREE
       };
 
       const job = createJob({ id: 1, title: "Job I Found" });
@@ -88,6 +110,7 @@ describe("getters", () => {
       expect(result).toEqual([job]);
       expect(INCLUDE_JOB_ORG).toHaveBeenCalledWith(job);
       expect(INCLUDE_JOB_TYPE).toHaveBeenCalledWith(job);
+      expect(INCLUDE_JOB_DEGREE).toHaveBeenCalledWith(job);
     });
   });
 });
--- src/store/getters.ts
+++ src/store/getters.ts
@@ -5,6 +5,7 @@ import {
   INCLUDE_JOB_ORG,
   INCLUDE_JOB_TYPE,
   UNIQUE_DEGREES,
+  INCLUDE_JOB_DEGREE,
 } from "@/store/constants";
 import { GlobalState } from "@/store/types";
 import { Job } from "@/api/types";
@@ -12,6 +13,7 @@ import { Job } from "@/api/types";
 interface IncludeJobGetters {
   INCLUDE_JOB_ORG: (job: Job) => boolean,
   INCLUDE_JOB_TYPE: (job: Job) => boolean,
+  INCLUDE_JOB_DEGREE: (job: Job) => boolean,
 }
 
 const getters = {
@@ -40,10 +42,15 @@ const getters = {
     if (state.selectedJobTypes.length === 0) return true;
     return state.selectedJobTypes.includes(job.jobType);
   },
+  [INCLUDE_JOB_DEGREE]: (state: GlobalState) => (job: Job) => {
+    if (state.selectedDegrees.length === 0) return true;
+    return state.selectedDegrees.includes(job.degree);
+  },
   [FILTERED_JOBS](state: GlobalState, getters: IncludeJobGetters) {
     return state.jobs
       .filter((job) => getters.INCLUDE_JOB_ORG(job))
-      .filter((job) => getters.INCLUDE_JOB_TYPE(job));
+      .filter((job) => getters.INCLUDE_JOB_TYPE(job))
+      .filter((job) => getters.INCLUDE_JOB_DEGREE(job));
   },
 };
 
