b223268b26be7fa56fa5900eb9e1e51d68086d24
kristen <kristen.schulte@huskers.unl.edu>
2022-07-31 15:00:48 -0500

    Convert job filters sidebar checkbox group to ts. Set up store type injection key.

--- src/assets/tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.ts
+++ src/assets/tests/unit/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.test.ts
@@ -1,9 +1,11 @@
 import { mount } from "@vue/test-utils";
 import { useStore } from "vuex";
 jest.mock("vuex");
+const useStoreMock = useStore as jest.Mock;
 
 import { useRouter } from "vue-router";
 jest.mock("vue-router");
+const useRouterMock = useRouter as jest.Mock;
 
 import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
 
@@ -33,9 +35,9 @@ describe("JobFiltersSidebarCheckboxGroup", () => {
 
   describe("when user clicks checkbox", () => {
     it("communicates that user has selected checkbox for value", async () => {
-      useRouter.mockReturnValue({ push: jest.fn() });
+      useRouterMock.mockReturnValue({ push: jest.fn() });
       const commit = jest.fn();
-      useStore.mockReturnValue({ commit });
+      useStoreMock.mockReturnValue({ commit });
       const props = {
         mutation: "SOME_MUTATION",
       };
@@ -46,20 +48,20 @@ describe("JobFiltersSidebarCheckboxGroup", () => {
       const clickableArea = wrapper.find("[data-test='clickable-area']");
       await clickableArea.trigger("click");
       const input1 = wrapper.find("[data-test='Value-A']");
-      await input1.setChecked();
+      await input1.setValue(true);
       expect(commit).toHaveBeenCalledWith("SOME_MUTATION", ["Value-A"]);
     });
   });
 
   it("navigates user to home job results page", async () => {
-    useStore.mockReturnValue({ commit: jest.fn() });
+    useStoreMock.mockReturnValue({ commit: jest.fn() });
     const push = jest.fn();
-    useRouter.mockReturnValue({ push });
+    useRouterMock.mockReturnValue({ push });
     const wrapper = mount(JobFiltersSidebarCheckboxGroup, createConfig());
     const clickableArea = wrapper.find("[data-test='clickable-area']");
     await clickableArea.trigger("click");
     const input1 = wrapper.find("[data-test='Value-A']");
-    await input1.setChecked();
+    await input1.setValue(true);
     expect(push).toHaveBeenCalledWith({ name: "JobResults" });
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ import {
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
 import router from "@/router";
-import store from "@/store";
+import store, { key } from "@/store";
 
 library.add(faSearch, faAngleDown, faAngleUp);
 createApp(App)
-  .use(store)
+  .use(store, key)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
